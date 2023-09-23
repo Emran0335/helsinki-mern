d1ed51cedb9d101386430dbe0e05eaa4d592aa00
Emran Hossain <emranhossain0335@gmail.com>
2023-09-24 00:12:55 +0600

    helsinki mern fullstack

---
+++ backend/model/note.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+mongoose.set("strictQuery", false);
+const url = process.env.MONGO_DB_URI;
+console.log("connected", url);
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connectiong to MongoDB:", error.message);
+  });
+
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  important: Boolean,
+  date: String,
+});
+
+noteSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Note", noteSchema);
